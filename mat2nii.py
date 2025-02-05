b65fc0e25cfa3954bc103db6c94cfe14363e6bb5
cshennju <cshen@smail.nju.edu.cn>
2025-02-05 14:53:22 +0800

    update

--- mat2nii.py
+++ mat2nii.py
@@ -6,7 +6,7 @@ import numpy as np
 
 path = os.path.join('C:/Users/ASUS/XXX.mat')
 mat = sio.loadmat(path) 
-rgb = np.squeeze(mat['gray'])
-new_image = nib.Nifti1Image(rgb,np.eye(4))
+gray = np.squeeze(mat['gray'])
+new_image = nib.Nifti1Image(gray,np.eye(4))
 save_path = os.path.join('C:/Users/ASUS/XXX.nii.gz')
 nib.save(new_image,save_path) 
